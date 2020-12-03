578599d1e84b12d01f1d25f5b75deac4373f96f9
Maxim Klochkov <m.klochkov@sap.com>
2020-12-03 13:54:41 +0300

    Webpack config updates

--- src/module.js
+++ src/module.js
@@ -1,1 +1,7 @@
 console.log('Module.js');
+
+async function start() {
+    return await Promise.resolve('Async working')
+}
+
+start().then(console.log)
--- webpack.config.js
+++ webpack.config.js
@@ -4,23 +4,33 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-console.log(path.resolve(__dirname, 'src'));
+const isProd = process.env.NODE_ENV === 'production';
+const isDev = !isProd;
+
+const getExtension = ext => isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`;
 
 module.exports = {
     context: path.resolve(__dirname, 'src'),
     mode: 'development',
-    entry: './index.js',
+    entry: ['@babel/polyfill', './index.js'],
     output: {
-        filename: 'bundle.[hash].js',
+        filename: getExtension('js'),
         path: path.resolve(__dirname, 'dist'),
     },
     resolve: {
         extensions: ['.js'],
         alias: {
             '@': path.resolve(__dirname, 'src'),
-            '@core': path.resolve(__dirname, 'src/core')
-        }
+            '@core': path.resolve(__dirname, 'src/core'),
+        },
     },
+    devtool: isDev ? 'source-map' : false,
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        port: 3333,
+        hot: isDev,
+    },
+    target: isDev ? 'web' : 'browserslist',
     plugins: [
         new CopyPlugin({
             patterns: [
@@ -32,17 +42,35 @@ module.exports = {
         }),
         new HtmlWebpackPlugin({
             template: 'index.html',
+            minify: {
+                removeComments: isProd,
+                collapseWhitespace: isProd,
+            },
         }),
         new CleanWebpackPlugin(),
         new MiniCssExtractPlugin({
-            filename: 'bundle.[hash].css'
-        })
+            filename: getExtension('css'),
+        }),
     ],
     module: {
         rules: [
             {
                 test: /\.css$/i,
                 use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
             },
         ],
     },
